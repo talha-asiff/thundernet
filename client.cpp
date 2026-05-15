613696a139f008556ad109cdf78c9aa24aab6ad9
Talha Asif <sp26-bse-064@cuilahore.edu.pk>
2026-05-15 15:34:27 +0500

    added network info functionality, server or client disconnection indicator, server side TXT log class

--- client.cpp
+++ client.cpp
@@ -3,9 +3,40 @@
 #include <winsock2.h>
 #include <conio.h>
 #include <fstream>
+#include <windows.h>
+#include <ctime>
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
+struct connectionInfo {
+	SOCKET acceptid;
+	string ipAddress;
+	int port;
+};
+string clientStart(){
+	time_t now = time(0);
+	time(&now);
+	string start = ctime(&now);
+	return start;
+}
+string clientStop(){
+	time_t now = time(0);
+	time(&now);
+	string stop = ctime(&now);
+	return stop;
+}
+bool serverexit(char *buffer){
+    if(strcmp(buffer, "exit") == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 int main(){
+    char exitin;
+    string start = "";
+    string stop = "";
     ofstream logFile("export/client_log.html");
 	logFile << "<!DOCTYPE html><html><head><title>Client Chat Log</title><link rel=\"stylesheet\" href=\"style.css\"></head>" << endl;
     string msg;
@@ -38,17 +69,21 @@ int main(){
     }
     else {
         cout << "Connected to the server! Reference ID: " << sock << std::endl;
+        start = clientStart();
+        logFile << "<h1 style = \"background-color: blue; color:white;\">Chat started at: " << start << "</h1><br>" << endl;
     }
     system("cls");
     cout<<"Enter a message (TYPE \"exit\" to quit): \n";
     while(1)
     {
-        
         cout<<"You : ";
         getline(cin, msg);
         send(sock, msg.c_str(), msg.length(), 0);
         if(msg == "exit"){
             cout<<"Exiting..."<<endl;
+            Sleep(2000);
+            stop = clientStop();
+            logFile << "<h1 style = \"background-color: blue; color:white;\">Chat ended at: " << stop << "</h1><br>" << endl; 
             break;
         }
         logFile << "<h2 class = \"client\"><strong>client:</strong> " << msg << "</h2>" << endl;
@@ -56,12 +91,34 @@ int main(){
         recv(sock, buffer, sizeof(buffer), 0);
         logFile << "<h2 class = \"server\"><strong>server:</strong> " << buffer << "</h2>" << endl;
         cout<<"Server : "<<buffer<<endl;
+        if(serverexit(buffer)){
+            cout<<"NOTICE : Server has disconnected."<<endl;
+        }
         
     }
 
     logFile.close();
     closesocket(sock);
     WSACleanup();
-    _getch();
+    system("cls");
+    cout<<"Disconnected from server."<<endl;
+    cout<<"Chat log saved to export/client_log.html"<<endl;
+    cout<<"connection started at : "<<start<<endl;
+    cout<<"connection ended at : "<<stop<<endl;
+    cout<<"Press 0 to get connection info, Press any key to exit..."<<endl;
+    exitin = _getch();
+    if(exitin == '0'){
+        connectionInfo info;
+        info.acceptid = sock;
+        info.ipAddress = "Localhost LAN";
+        info.port = 5555;
+        cout<<"Connection Info:\n";
+        cout<<"Mode : TCP\n Computer Type : Client\n Data mode : Stream\n";
+        cout<<"Accepting Socket Reference ID: "<<info.acceptid<<endl;
+        cout<<"Live at: "<<info.ipAddress<<endl;
+        cout<<"Port: "<<info.port<<endl;
+        cout<<"Press any key to exit..."<<endl;
+        _getch();
+    }
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -4,9 +4,59 @@
 #include <string>
 #include <windows.h>
 #include <fstream>
-#pragma comment(lib, "ws2_32.lib")
+#include <ctime>
+#pragma comment(lib, "ws2_32.lib") //Required for GCC compiler to link the Winsock library
 using namespace std;
-int server(){
+class networkInfo {
+	public:
+		string platform;
+		int port;
+		string stime;
+		string etime;
+		void txtlog(string platform, int port, string stime, string etime);
+		networkInfo(string live, int p, string s, string e){
+			this->platform = live;
+			this->port = p;
+			this->stime = s;
+			this->etime = e;
+	}
+};
+void networkInfo :: txtlog(string platform, int port, string stime, string etime){
+	ofstream logFile("export/network_log.txt", ios::app);
+	logFile << "Network logs are only stored on server side (administrator) \nPlatform: " << platform << "\nPort: " << port << "\nStart Time: " << stime << "\nEnd Time: " << etime << "\n-------------------------\n";
+	logFile.close();
+}
+struct connectionInfo {
+	SOCKET connectid;
+	SOCKET acceptid;
+	string ipAddress;
+	int port;
+};
+string serverStart(){
+	time_t now = time(0);
+	time(&now);
+	string start = ctime(&now);
+	return start;
+}
+string serverStop(){
+	time_t now = time(0);
+	time(&now);
+	string stop = ctime(&now);
+	return stop;
+}
+bool clientexit(char *buffer){
+	if(strcmp(buffer, "exit") == 0){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int main(){
+	string start = "";
+	string stop = "";
+	char exitin;
 	system("cls");
 	ofstream logFile("export/server_log.html");
 	logFile << "<!DOCTYPE html><html><head><title>Server Chat Log</title><link rel=\"stylesheet\" href=\"style.css\"></head>" << endl;
@@ -53,6 +103,8 @@ int server(){
 	else {
 		cout << "A friend has connected! Reference ID: " << acceptsock <<endl;
 	}
+	start = serverStart();
+	logFile << "<h1 style = \"background-color: blue; color:white;\">Chat started at: " << start << "</h1><br>" << endl;
 	Sleep(3000);
 	system("cls");
 	cout<<"Enter a message (SEND \"exit\" to quit):\n ";
@@ -62,21 +114,45 @@ int server(){
 		recv(acceptsock, buffer, sizeof(buffer), 0);
 		 logFile << "<h2 class = \"client\"><strong>Client:</strong> " << buffer << "</h2>" <<endl;
          cout<<"Client : "<<buffer<<endl;
+		 if(clientexit(buffer)){
+			cout<<"NOTICE : Client has disconnected."<<endl;
+		 }
 		 cout<<"You : ";
          getline(cin, msg);
 		 send(acceptsock, msg.c_str(), msg.length(), 0);
 		 if(msg == "exit"){
 			cout<<"Exiting..."<<endl;
+			stop = serverStop();
+			logFile << "<h1 style = \"background-color: blue; color:white;\">Chat ended at: " << stop << "</h1><br>" << endl;	
 			break;
 		 }
 		 logFile << "<h2 class = \"server\"><strong>Server:</strong> " << msg << "</h2>" << endl;
     }
 	logFile.close();
 	WSACleanup();
-	_getch();
+	system("cls");
+	cout<<"Disconnected from client."<<endl;
+	cout<<"Chat log saved to export/server_log.html"<<endl;
+	cout<<"connection started at : "<<start<<endl;
+	cout<<"connection ended at : "<<stop<<endl;
+	cout<<"Press 0 to get connection info, Press any key to exit..."<<endl;
+	exitin = _getch();
+	if(exitin == '0'){
+		connectionInfo info;
+		info.connectid = sock;
+		info.acceptid = acceptsock;
+		info.ipAddress = "127.0.0.1";
+		info.port = ntohs(ser.sin_port);
+		cout<<"Connection Info:\n";
+		cout<<"MOde : TCP\n Computer Type : Server\n Data mode : Stream\n";
+		cout<<"Listening socket Reference ID: "<<info.connectid<<endl;
+		cout<<"Accepting socket Reference ID: "<<info.acceptid<<endl;
+		cout<<"Live at: "<<info.ipAddress<<endl;
+		cout<<"Port: "<<info.port<<endl;
+		cout<<"Press any key to exit..."<<endl;
+		_getch();
+		networkInfo netinfo(info.ipAddress, info.port, start, stop);
+		netinfo.txtlog(netinfo.platform, netinfo.port, netinfo.stime, netinfo.etime);
+	}
 	return 0;
-}
-
-int main(){
-	server();
 }
